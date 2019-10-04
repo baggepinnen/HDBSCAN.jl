f4560579c131700faa17c04fd73b6868dea8f2b3
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2019-10-04 10:34:28 +0800

    init issues solved

--- src/HDBSCAN.jl
+++ src/HDBSCAN.jl
@@ -2,11 +2,10 @@ module HDBSCAN
 using PyCall, Clustering
 export hdbscan, probabilities, exemplars, outlier_scores, leaf_size
 
-hdbs = pyimport("hdbscan")
+const hdbs = PyNULL()
 
-struct HdbscanResult <: ClusteringResult
-    clusterer
-    assignments
+function __init__()
+    @eval global hdbs = pyimport("hdbscan")
 end
 
 """
@@ -14,10 +13,19 @@ end
 hdbscan(min_cluster_size=5, min_samples=min_cluster_size, metric="euclidean", alpha=1.0, p=nothing, algorithm="best", leaf_size=40, memory=Memory(location=nothing), approx_min_span_tree=true, gen_min_span_tree=false, core_dist_n_jobs=4, cluster_selection_method="eom", allow_single_cluster=false, prediction_data=false, match_reference_implementation=false)
 """
 function hdbscan(X; min_cluster_size=5, kwargs...)
+    @show hdbs
     clusterer = hdbs.HDBSCAN(min_cluster_size=min_cluster_size, kwargs...)
     cluster_labels = clusterer.fit_predict(X')
     HdbscanResult(clusterer, cluster_labels .+ 1)
 end
+
+struct HdbscanResult <: ClusteringResult
+    clusterer
+    assignments
+end
+
+# @show hdbs.HDBSCAN(randn(2,100))
+
 
 probabilities(cr::HdbscanResult) = cr.clusterer.probabilities_
 exemplars(cr::HdbscanResult) = cr.clusterer.exemplars_
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,16 @@
-using HDBSCAN, Test
+using Test
+using HDBSCAN
+
 
 
 @testset "HDBSCAN" begin
+
+X = [randn(2,10000) fill(5,2).*randn(2,10000)]
+
+result = hdbscan(X, min_cluster_size=60)
+
+probabilities(result)
+exemplars(result)       # Computed at first call, this takes long time
+outlier_scores(result)
 
 end
